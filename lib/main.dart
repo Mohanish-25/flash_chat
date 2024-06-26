ba22063f52c81269d91f696809db63b7c533440e
Mohanish Desale <mohanishdesale@gmail.com>
2024-06-26 23:49:28 +0530

    Added registration with email and password

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(FlashChat());
+final apiKey = dotenv.env['API_KEY'];
+final appId = dotenv.env['APP_ID'];
+final messagingSenderId = dotenv.env['MESSAGING_SENDER_ID'];
+final projectId = dotenv.env['PROJECT_ID'];
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: apiKey ?? '',
+      appId: appId ?? '',
+      messagingSenderId: messagingSenderId ?? '',
+      projectId: projectId ?? '',
+    ),
+  );
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
